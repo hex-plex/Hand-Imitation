8f16c3e465ee6a3720c2726cb098f509782a03f2
Saaswath <54203063+infini8-13@users.noreply.github.com>
2020-06-11 22:05:28 +0530

    Update handOfJustice_env.py

--- Simulation/handOfJustice/handOfJustice_env.py
+++ Simulation/handOfJustice/handOfJustice_env.py
@@ -55,25 +55,6 @@ class HandOfJustice(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def handmask(frame):
-        frame=cv2.flip(frame,1)
-        kernel = np.ones((3,3),np.uint8)
-        #define region of interest
-        roi=frame[100:400, 100:300]
-        cv2.rectangle(frame,(100,100),(300,400),(0,255,0),0)
-        lab = cv2.cvtColor(roi, cv2.COLOR_BGR2LAB)
-        # define range of skin color in lab
-        lower_skin = np.array([40,120,120], dtype=np.uint8)
-        upper_skin = np.array([255,170,170], dtype=np.uint8)
-        #extract skin colour image
-        mask = cv2.inRange(lab, lower_skin, upper_skin)
-        #blur the image
-        cr_frame=frame[100:400,100:300]
-        cv2.imshow('mask',mask)
-        cv2.imshow('frame',frame)
-        cv2.imshow("cropped",cr_frame)
-        return mask
-        
     def getImage(flag=True):
         """
         this function returns hand image in RGBA format
