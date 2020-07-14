94e0153d564c7c16cf66817411764049081517f5
hex-plex <sendhilsom7@gmail.com>
2020-07-14 19:02:18 +0530

    update

--- Simulation/RL-Test.py
+++ Simulation/RL-Test.py
@@ -3,7 +3,10 @@ import gym_handOfJustice
 import cv2
 from stable_baselines.sac import SAC
 from stable_baselines.sac.policies import LnCnnPolicy
-env = gym.make("handOfJustice-v0",cap=cv2.VideoCapture(<--directory-->),epsilon=150)
+strea = cv2.VideoCapture("C:/Users/somna/Desktop/handOfJustice/Simulation/dataset/%06d.png")
+if not strea.isOpened():
+    raise Exception("Problem exporting the video stream")
+env = gym.make("handOfJustice-v0",cap=strea,epsilon=150)
 
 model = SAC(LnCnnPolicy, env , verbose=1,tensorboard_log="./logs")
 model.learn(total_timesteps=50000,log_interval=10)
