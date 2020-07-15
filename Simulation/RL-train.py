e1f016dd9c3b008bed6c66f7c9cffa0ef0118052
hex-plex <sendhilsom7@gmail.com>
2020-07-16 01:31:14 +0530

    Error fixes

--- Simulation/RL-train.py
+++ Simulation/RL-train.py
@@ -1,15 +1,17 @@
 import tensorflow as tf
-from keras.callbacks import TensorBoard
-from keras.layers import Input,Dense,Conv2D,MaxPooling2D,Flatter
-from keras.models import Model
-from keras.optimizers import Adam
-from keras import backend as K
-from keras.application.mobilenet_v2 as MobileNetv2
+from tensorflow import keras
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.layers import Input,Dense,Conv2D,MaxPooling2D,Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import backend as K
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+import numpy as np
 import os
 import gym
 import pybullet
 import cv2
-import gym-handOfJustice
+import gym_handOfJustice
 
 hyperparam={
     'clip_val':0.2,
@@ -34,9 +36,9 @@ def advantages(valueS,masks,rewardSA):
     adv = np.array(returns) - valueS[:-1]
     return returns, ((adv -np.mean(adv))/(np.std(adv)+1e-10))
 
-def ppo_loss_actor(delta):
+def ppo_loss_actor(delta,norm_dist):
     def loss(y_true,y_pred):
-        action,norm_dist = y_pred
+        action = y_pred
         total_loss= -1*K.log(norm_dist.prob(action))*delta
         return total_loss
     return loss
@@ -57,7 +59,7 @@ def ppo_loss_np(old_policy_probs,advantages,rewards,valueS):
     return loss
 
 def model_actor_image(input_dims, output_dims):
-    state_input = Input(shape=input_dim)
+    state_input = Input(shape=input_dims)
     delta = Input(shape=(1,1,))
     feature_image = MobileNetV2(include_top=False, weights='imagenet')
     for layer in feature_image.layers:
@@ -65,17 +67,17 @@ def model_actor_image(input_dims, output_dims):
 
     x = Flatten(name = 'flatten')(feature_image(state_input))
     x = Dense(512,activation='relu' , name='fc1')(x)
-    x = Dense(128,activation='relu' , name='fc1')(x)
-    mu = Dense(n_actions)(x)
-    sigma = Dense(n_actions)(x)
+    x = Dense(128,activation='relu' , name='fc2')(x)
+    mu = Dense(len(output_dims))(x)
+    sigma = Dense(len(output_dims))(x)
     sigma = tf.keras.activations.softplus(sigma)+1e-5
-    norm_dist = tf.contrib.distribution.Normal(mu,sigma)
+    norm_dist = tf.contrib.distributions.Normal(mu,sigma)
     action_tf_var = tf.squeeze(norm_dist.sample(1),axis=0)
     
 
     model = Model(inputs=[state_input, delta],
-                  outputs=[action_tf_var,norm_dist])
-    model.compile(optimizer=Adam(lr=hyperparam['actor_lr']),loss=[ppo_loss_actor(delta=delta)])
+                  outputs=[action_tf_var])
+    model.compile(optimizer=Adam(lr=hyperparam['actor_lr']),loss=[ppo_loss_actor(delta,norm_dist)])
     model.summary()
     return model
 
@@ -84,49 +86,51 @@ def model_critic_image(input_dims):
     feature_image = MobileNetV2(include_top=False, weights='imagenet')
     for layer in feature_image.layers:
         layer.trainable=False
-
-    x = Flatten(name='flatten')(feature_image)
+    x = Flatten(name='flatten')(feature_image(state_input))
     x = Dense(512,activation='relu',name='fc1')(x)
-    x = Dense(128,activation='relu',name='fc1')(x)
+    x = Dense(128,activation='relu',name='fc2')(x)
     Value_function = Dense(1)(x)
 
     model = Model(inputs=[state_input],outputs=[Value_function])
-    model.compile(optimizer=Adam(lr=hyperparam['critic_lr'],loss='mse'))
+    model.compile(optimizer=Adam(lr=hyperparam['critic_lr']),loss='mse')
     model.summary()
     return model
 
 
-tensor_board = TensorBoard(log_dir=os.getcwd()+'/logs')
-
-env = gym.make('handOfJustice-v0',cap=cv2.VideoCapture(<--directory-->))
-
+tensor_board = TensorBoard(log_dir=os.getcwd()+'\\logs\\')
+strea = cv2.VideoCapture(os.getcwd()+"\\dataset\\%06d.png")
+if not strea.isOpened():
+    raise Exception("Problem exporting the video stream")
+env = gym.make('handOfJustice-v0',cap=strea,epsilon=200)
 state_dims = env.observation_space.shape
 action_dims = env.action_space.shape
 
 actor_model = model_actor_image(input_dims=state_dims,output_dims=action_dims)
 critic_model = model_critic_image(input_dims=state_dims)
 
-dummy_n = np.zeros((1, 1, action_dims))
+dummy_n = np.zeros((1, 1, len(action_dims)))
 dummy_1 = np.zeros((1, 1, 1))
 best_reward= -10000000000
 num_episodes= 50000
 episode_history=[]
 for episode in range(num_episodes):
     state = env.reset()
+    state = state.reshape((1,)+state.shape  )
     reward_total = 0
     step = 0
     done=False
     while not done:
-        action,norm_dist = model_actor.predict([state,dummy_1],steps=1)
+        action = actor_model.predict([state,dummy_1],steps=1)
         next_state,reward,done,_=env.step(np.squeeze(action))
+        next_state = next_state.reshape((1,) + next_state.shape )
         step+=1
         reward_total +=reward
-        V_of_next_state = model_critic.predict(next_state)
-        V_this_state = model_critic.predict(state)
+        V_of_next_state = critic_model.predict(next_state)
+        V_this_state = critic_model.predict(state)
         target = reward + hyperparam['gamma']*np.squeeze(V_of_next_state)
         td_error = target - np.squeeze(V_this_state)
-        model_actor.fit([state,td_error],[dummy_n,dummy_n],callbacks=[tensor_board])## THis step can be done over a batch periodically also
-        model_critic.fit([state],[target],callbacks=[tensor_board])
+        actor_model.fit([state,td_error],[dummy_n,dummy_n],callbacks=[tensor_board])## THis step can be done over a batch periodically also
+        critic_model.fit([state],[target],callbacks=[tensor_board])
         state=next_state
     episode_history.append(reward_total)
     print("Episode: {}, Number of Steps : {}, Cumulative reward: {:0.2f}".format(
